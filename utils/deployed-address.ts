8fb0898e973767ab1f09db581002a4315bda8d41
colony airdrops <colonyairdrops@gmail.com>
2024-08-11 06:56:47 +0000

    Initial commit

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0x09d9D09fe5d361A55cb78A7682F3e9E55f91Ff3F'
-export const ProxyAdmin = '0x58dBCe6301413822E9B3F1c4cFc42D2c750CaAb2'
-export const SWTRImplementation = '0xa623de330d992eB698d56c271b2cc1e7e3D958B5'
+export const SWTRProxy = '0x3701598788D1D87e77d85a0ADa3A83C005F5D690'
+export const ProxyAdmin = '0x377E36c504f08aFa521EEe532610cDB7511c228F'
+export const SWTRImplementation = '0x4C1De141e3ef95bE2b3Ce1410DfBA9A15deA597F'
     
